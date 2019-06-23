c8d8ed7c340da98db1c24f2867c4435bae0cef2d
Mihai Ene <rizulica@gmail.com>
2019-06-23 18:15:02 +0200

    A finished

--- DellChallenge/DellChallenge.A/Program.cs
+++ DellChallenge/DellChallenge.A/Program.cs
@@ -9,6 +9,18 @@ namespace DellChallenge.A
             // State and explain console output order.
             new B();
             Console.ReadKey();
+
+            /*
+            Line 1: A.A()
+            Class 'B' inherits class 'A'. When a new instance of `B` is initialized, the constructor of 'A' will be called first. This constructor calls 'Console.WriteLine("A.A()");', which is what is outputted first.
+
+            Line 2: B.B()
+            After the constructor of the parent class is called, the constructor of the child class, 'B', follows. This constructor calls 'Console.WriteLine("B.B()");', which is outputted second.
+
+            Line 3: A.Age
+            The code to output this text is inside the setter of the 'Age' property of the 'A' class. After line 2 is outputted from B's constructor, 'Age = 0;' is executed.
+            Since the Age property is inherited by B from its parent class, A, setting a new value will access the property's setter, which contains 'Console.WriteLine("A.Age");'.
+            */
         }
     }
 
